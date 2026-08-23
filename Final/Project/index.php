d0adf0e22597463f25cee760ab7fc0ac3ca0cc64
Talha Faiyaz <talhafaiyazofficial@gmail.com>
2026-08-23 16:10:31 +0600

    MVC

--- Final/Project/index.php
+++ Final/Project/index.php
@@ -1,8 +1,11 @@
 <?php
-
-
 session_start();
-
 require 'controllers/RegistrationController.php';
 
-?>
+$controller = new RegistrationController();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $controller->store();
+} else {
+    $controller->index();
+}
